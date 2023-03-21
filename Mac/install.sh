c6967a1480ed9d4a174d79fd2041433bcb887b6c
lnwor <43646324+lnwor@users.noreply.github.com>
2023-03-21 14:37:15 +0100

    corrected install script

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+set -e
+
 echo -n "Input your WifiCity username: "
 read -r USERNAME
 echo -n "Input your WifiCity password: "
 read -r PASSWORD
 
-sed -i "s/USERNAME=\"\"/USERNAME=\"$USERNAME\"/" ./login_wificity.sh
-sed -i "s/PASSWORD=\"\"/PASSWORD=\"$PASSWORD\"/" ./login_wificity.sh
-sed -i "s/SOMEWHERE/\/usr\/local\/bin/" ./me.wificity.login.plist
+sed -i.bu "s/USERNAME=\"\"/USERNAME=\"$USERNAME\"/" ./login_wificity.sh
+sed -i.bu "s/PASSWORD=\"\"/PASSWORD=\"$PASSWORD\"/" ./login_wificity.sh
+sed -i.bu "s/SOMEWHERE/\/usr\/local\/bin/" ./me.wificity.login.plist
 
+sudo mkdir -p /usr/local/bin/
 sudo cp ./login_wificity.sh /usr/local/bin/
 cp ./me.wificity.login.plist ~/Library/LaunchAgents/
 launchctl load -w ~/Library/LaunchAgents/me.wificity.login.plist
